37acbbed6840131974d67dd2a1ff12c16aae1b9c
nhathaocute <nhathao0945477411@gmail.com>
2024-03-26 22:54:16 +0700

    Tao trang them lien he

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createApp(App).mount("#app");
-
 import router from "./router";
+createApp(App).mount("#app");
 createApp(App).use(router).mount("#app");
